f788a932d24b02bc0b0d36cfec67dab0f8dbe6de
UltimateHG <26477993+UltimateHG@users.noreply.github.com>
2025-01-24 14:48:52 +0800

    Finish Add, List features

--- src/main/java/uhgbot.java
+++ src/main/java/uhgbot.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class uhgbot {
     public static void main(String[] args) {
@@ -8,6 +9,7 @@ public class uhgbot {
                           " What can I do for you?\n" +
                           "____________________________________________________________";
         
+        ArrayList<String> tasks = new ArrayList<>();
         System.out.println(welcomeMsg);
         
         Scanner scanner = new Scanner(System.in);
@@ -15,14 +17,22 @@ public class uhgbot {
         
         while (isRunning) {
             String input = scanner.nextLine();
-            
             System.out.println("____________________________________________________________");
             
             if (input.toLowerCase().equals("bye")) {
                 System.out.println(" Bye. Hope to see you again soon!");
                 isRunning = false;
+            } else if (input.toLowerCase().equals("list")) {
+                if (tasks.isEmpty()) {
+                    System.out.println(" No tasks in the list!");
+                } else {
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    }
+                }
             } else {
-                System.out.println(" " + input);
+                tasks.add(input);
+                System.out.println(" added: " + input);
             }
             
             System.out.println("____________________________________________________________");

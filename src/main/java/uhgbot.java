1e511e52787267b6f3ba809171ce74a80bf45790
UltimateHG <26477993+UltimateHG@users.noreply.github.com>
2025-01-24 14:44:04 +0800

    Update gitignore and finish level 0

---
+++ src/main/java/uhgbot.java
@@ -0,0 +1,12 @@
+public class uhgbot {
+    public static void main(String[] args) {
+        String name = "uhgbot";
+        String intro = "____________________________________________________________\r\n" + //
+                        " Hello! I'm " + name + "\r\n" + //
+                        " What can I do for you?\r\n" + //
+                        "____________________________________________________________\r\n" + //
+                        " Bye. Hope to see you again soon!\r\n" + //
+                        "____________________________________________________________";
+        System.out.println(intro);
+    }
+}

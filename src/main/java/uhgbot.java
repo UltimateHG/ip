4d0c3903e0e9ef05f1dc16fd3da207d3434868f3
UltimateHG <26477993+UltimateHG@users.noreply.github.com>
2025-01-24 14:46:46 +0800

    Make bot echo user input

--- src/main/java/uhgbot.java
+++ src/main/java/uhgbot.java
@@ -1,12 +1,33 @@
+import java.util.Scanner;
+
 public class uhgbot {
     public static void main(String[] args) {
         String name = "uhgbot";
-        String intro = "____________________________________________________________\r\n" + //
-                        " Hello! I'm " + name + "\r\n" + //
-                        " What can I do for you?\r\n" + //
-                        "____________________________________________________________\r\n" + //
-                        " Bye. Hope to see you again soon!\r\n" + //
-                        "____________________________________________________________";
-        System.out.println(intro);
+        String welcomeMsg = "____________________________________________________________\n" +
+                          " Hello! I'm " + name + "\n" +
+                          " What can I do for you?\n" +
+                          "____________________________________________________________";
+        
+        System.out.println(welcomeMsg);
+        
+        Scanner scanner = new Scanner(System.in);
+        boolean isRunning = true;
+        
+        while (isRunning) {
+            String input = scanner.nextLine();
+            
+            System.out.println("____________________________________________________________");
+            
+            if (input.toLowerCase().equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                isRunning = false;
+            } else {
+                System.out.println(" " + input);
+            }
+            
+            System.out.println("____________________________________________________________");
+        }
+        
+        scanner.close();
     }
 }
